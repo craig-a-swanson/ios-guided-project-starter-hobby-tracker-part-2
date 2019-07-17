4a9b3bd201df4e895e3c2f6a638637689a3139b6
Ben Gohlke <jcgohlke@me.com>
2019-07-17 10:08:17 -0400

    Initial commit

---
+++ HobbyTracker/FriendsTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FriendsTableViewController.swift
+//  HobbyTracker
+//
+//  Created by Ben Gohlke on 7/16/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class FriendsTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
